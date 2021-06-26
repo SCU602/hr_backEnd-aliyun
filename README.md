14be5709101931c0e1e6d4625178b919aa4d4409
mr.somebody <mr.somebody@mrsomebodydeMacBook-Air.local>
2021-06-26 09:36:34 +0800

    阿里云最终版

---
+++ README.md
@@ -0,0 +1,8 @@
+# hr_backEnd（阿里云版）
+
+[CSDN准备工作说明](https://blog.csdn.net/nuaa042216/article/details/112504407?utm_medium=distribute.pc_relevant_download.none-task-blog-2~default~BlogCommendFromBaidu~default-2.nonecase&depth_1-utm_source=distribute.pc_relevant_download.none-task-blog-2~default~BlogCommendFromBaidu~default-2.nonecas)
+
+安装redis后需要配置一下密码为空
+
+vue需要vue3.x版本，不能是vue2.x版本
+
---
+++ hr/src/main/java/org/hr/HrApplication.java
@@ -0,0 +1,17 @@
+package org.hr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityFilterAutoConfiguration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableScheduling
+public class HrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HrApplication.class, args);
+    }
+
+}
